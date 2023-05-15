da1110ed4b07929a5951d6805b413296b0b46faa
IngridLindor <ingridsantiagolindor@gmail.com>
2023-05-15 17:27:52 -0300

    continuaçao dos exercicios

--- lista_aula_2.py
+++ lista_aula_2.py
@@ -1,20 +1,101 @@
 # Lista de desafios da aula 2
 
 # COLETA E AMONSTRAGEM DE DADOS
-# Crie um programa que solicite à pessoa usuária digitar seu nome, e imprima “Olá, [nome]!”.
-# Crie um programa que solicite à pessoa usuária digitar seu nome e idade, e imprima “Olá, [nome], você tem [idade] anos.”.
-# Crie um programa que solicite à pessoa usuária digitar seu nome, idade e altura em metros, e imprima “Olá, [nome], você tem [idade] anos e mede [altura] metros!”.
+print ('EX 1. Crie um programa que solicite à pessoa usuária digitar seu nome, e imprima “Olá, [NOME]!”')
+nome=input ('Digite seu nome: ')
+print (f'ola {nome}')
+
+print('EX 2.Crie um programa que solicite à pessoa usuária digitar seu nome e idade, e imprima “Olá, [nome], você tem [idade] anos.”.')
+nome=input('Digite seu nome: ')
+idade=input("Digite a sua idade: ")
+print(f'Olá,{nome} voce tem {idade} anos!')
+
+print('EX 3.Crie um programa que solicite à pessoa usuária digitar seu nome, idade e altura em metros, e imprima “Olá, [nome], você tem [idade] anos e mede [altura] metros!”.')
+nome=input('Digite seu nome: ')
+idade=input("Digite a sua idade: ")
+altura=input('Digite sua altura: ')
+print(f'Olá {nome} voce tem {idade} anos e {altura} metros!')
 
 # CALCULADORA COM OPERADORES
-# Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a soma dos dois valores.
-# Crie um programa que solicite três valores numéricos à pessoa usuária e imprima a soma dos três valores.
-# Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a subtração do primeiro pelo o segundo valor.
-# Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a multiplicação dos dois valores.
-# Crie um programa que solicite dois valores numéricos, um numerador e um denominador, e realize a divisão entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.
-# Crie um programa que solicite dois valores numéricos, um operador e uma potência, e realize a exponenciação entre esses dois valores.
-# Crie um programa que solicite dois valores numéricos, um numerador e um denominador e realize a divisão inteira entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.
-# Crie um programa que solicite dois valores numéricos, um numerador e um denominador, e retorne o resto da divisão entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.
-# Crie um código que solicita 3 notas de um estudante e imprima a média das notas.
+print('EX 4.Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a soma dos dois valores.')
+n1=int(input('Digite um valor: '))
+n2=int(input('Digite outro valor: '))
+result= n1+n2
+print(f'{n1} + {n2} = {result}')
+
+
+print('EX 5. Crie um programa que solicite três valores numéricos à pessoa usuária e imprima a soma dos três valores.')
+n1=int(input('Digite um valor: '))
+n2=int(input('Digite outro valor: '))
+n3=int(input('Digite outro valor: '))
+result= n1+n2+n3
+print(f'{n1} + {n2} + {n3} = {result}')
+
+print('EX 6.Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a subtração do primeiro pelo o segundo valor.')
+n1=int(input('Digite um valor: '))
+n2=int(input('Digite outro valor: '))
+result= n1-n2
+print(f'{n1} - {n2} = {result}')
+
+print('EX 7. Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a multiplicação dos dois valores.')
+n1=int(input('Digite um valor: '))
+n2=int(input('Digite outro valor: '))
+result= n1 * n2
+print(f'{n1} X {n2} = {result}')
+
+print('EX 8. Crie um programa que solicite dois valores numéricos, um numerador e um denominador, e realize a divisão entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.')
+num=int(input('Digite um numero'))
+den=int(input('Digite outro valor (esse valor NAO pode ser zero):'))
+if den == 0:
+    print('o segundo valor nao pode ser zero')
+else:
+    result = num/den
+    print(f'{num} / {den} = {result}')
+    
+
+
+print('EX 9.Crie um programa que solicite dois valores numéricos, um operador e uma potência, e realize a exponenciação entre esses dois valores.')
+ope=int(input('Digite o operador'))
+pot=int(input('Digite a potência:'))
+
+result = ope ** pot
+contador=0
+
+print(result)
+
+print('EX 10. Crie um programa que solicite dois valores numéricos, um numerador e um denominador e realize a divisão inteira entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.') 
+num=int(input('Digite o numerador:'))
+den=int(input('Digite o denominador:'))
+if den == 0:
+    print(' O segundo valor nao pode ser igual a zero. Por favor tente novamente')
+
+else:
+    resto = num / den 
+
+    print(f' A divisão entre {num} e {den} é {resto}')
+
+print('EX 11.Crie um programa que solicite dois valores numéricos, um numerador e um denominador, e retorne o resto da divisão entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.')
+num=float(input('Digite o numerador:'))
+den=float(input('Digite o denominador:'))
+if den == 0:
+    print(' O segundo valor nao pode ser igual a zero. Por favor tente novamente')
+
+else:
+    resto = num % den 
+
+    print(f' o resto da divisão entre {num} e {den} é {resto}')
+
+print('Crie um código que solicita 3 notas de um estudante e imprima a média das notas.') 
+n1=float(input(' Digite a primeira nota: '))
+n2=float(input(' Digite a segunda nota: '))
+n3=float(input(' Digite a terceira nota: '))
+
+media = (n1+n2+n3) /3 
+media = round(media,2)
+
+print( f'A média entre esses valores é igual a {media}')
+
+
 # Crie um código que calcule e imprima a média ponderada dos números 5, 12, 20 e 15 com pesos respectivamente iguais a 1, 2, 3 e 4.
 
 # EDITANDO TEXTOS
